543ff3695a6169bbee0b1f17ea194da40c1850f9
d.boendermaker <d.boendermaker@narz.net>
2023-02-10 10:16:49 +0100

    blubb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './views/index/index.component';
 import { TableformComponent } from './views/tableform/tableform.component';
 import { DrawermodalComponent } from './views/drawermodal/drawermodal.component';
-import { TreeviewComponent } from './views/treeview/treeview.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'index', component: IndexComponent },
   { path: 'tableform', component: TableformComponent },
-  { path: 'drawermodal', component: DrawermodalComponent },
-  { path: 'treeview', component: TreeviewComponent }
+  { path: 'drawermodal', component: DrawermodalComponent }
 ];
 
 @NgModule({
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@app/material.module';
 import { DrawermodaldialogModule } from '@app/components/drawermodaldialog/drawermodaldialog.module';
-
+import { MultidimensionalViewModule } from '@app/components/multidimensional-view/multidimensional-view.module';
 
 
 @NgModule({
@@ -10,10 +10,12 @@ import { DrawermodaldialogModule } from '@app/components/drawermodaldialog/drawe
   imports: [
     CommonModule,
     MaterialModule,
-    DrawermodaldialogModule
+    DrawermodaldialogModule,
+    MultidimensionalViewModule
   ],
   exports: [
-    DrawermodaldialogModule
+    DrawermodaldialogModule,
+    MultidimensionalViewModule
   ]
 })
 export class ComponentsModule { }
