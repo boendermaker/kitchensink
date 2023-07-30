e52f17c8a03ed74bfcf4dc90bb9db4c6e0eb7ce4
boendermaker <elektrobong@googlemail.com>
2023-07-30 20:06:35 +0200

    Loading state to template added

--- src/app/views/recursivelazy/interface.ts
+++ src/app/views/recursivelazy/interface.ts
@@ -1,15 +1,16 @@
-export interface ITreeData {
-    id?: string,
-    level?: number,
-    collapsed?: boolean,
-    type?: 'group'|'item',
-    label?: string,
-    children?: any[]
-}
-
 export interface INodeData {
   id: string,
   parentid: string,
   type: 'group'|'item',
   label: string
+}
+
+export interface ITreeData {
+    id?: string,
+    level?: number,
+    collapsed?: boolean,
+    loading?: boolean,
+    type?: 'group'|'item',
+    label?: string,
+    children?: any[]
 }
