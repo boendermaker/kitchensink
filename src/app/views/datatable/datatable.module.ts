8c66f89f2c52863faa677a9c14a314afcf4e58a5
boendermaker <elektrobong@googlemail.com>
2023-08-17 00:56:57 +0200

    click on table row added

--- src/app/views/datatable/datatable.module.ts
+++ src/app/views/datatable/datatable.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatatableComponent } from './datatable.component';
-import { MaterialModule } from '../../material.module';
+import { AllAngularMaterialMDCModulesModule } from '../../shared/modules/allmaterial/allmaterial.module';
 
 
 @NgModule({
@@ -10,7 +10,7 @@ import { MaterialModule } from '../../material.module';
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    AllAngularMaterialMDCModulesModule
   ]
 })
 export class DatatableModule { }
