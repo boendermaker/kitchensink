d41be6aa707113ecf14d18377aa5f320e0e30f8c
boendermaker <elektrobong@googlemail.com>
2023-11-15 21:29:56 +0100

    demostate extendable added

---
+++ src/app/state/demo.state.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { StateBaseService } from "./state.base.class";
+
+interface IDemo {
+  id: number;
+  label: string;
+}
+
+interface IDemoState {
+  demos: IDemo[];
+  selectedDemoId: number;
+}
+
+const initialState: IDemoState = {
+  demos: [],
+  selectedDemoId: undefined
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DemoStateService extends StateBaseService<IDemoState>{
+  todos$: Observable<IDemo[]> = this.select(state => state.demos);
+
+  selectedTodo$: Observable<IDemo> = this.select((state) => {
+    return state.demos.find((item) => item.id === state.selectedDemoId);
+  });
+
+  constructor() {
+    super(initialState);
+  }
+
+  addDemo(demo: IDemo) {
+    this.setState({demos: [...this.state.demos, demo]})
+  }
+
+  selectDemo(demo: IDemo) {
+    this.setState({ selectedDemoId: demo.id });
+  }
+}
