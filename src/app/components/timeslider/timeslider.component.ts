17c0fd7dad8d86178e4dc40568248774599a5941
d.boendermaker@narz.net <d.boendermaker@narz.net>
2024-04-17 08:28:02 +0200

    timslider component added

---
+++ src/app/components/timeslider/timeslider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timeslider',
+  standalone: true,
+  imports: [],
+  templateUrl: './timeslider.component.html',
+  styleUrl: './timeslider.component.scss'
+})
+export class TimesliderComponent {
+
+}
