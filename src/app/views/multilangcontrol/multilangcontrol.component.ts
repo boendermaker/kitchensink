c56025c37c903315fb8518afeed476c8d8459bcb
boendermaker <elektrobong@googlemail.com>
2024-09-15 22:56:00 +0200

    lodash added, path generator added

--- src/app/views/multilangcontrol/multilangcontrol.component.ts
+++ src/app/views/multilangcontrol/multilangcontrol.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
 import { MultilanginputComponent } from "../../components/multilanginput/multilanginput.component";
 import { AsyncValidatorFn, FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AllAngularMaterialMDCModulesModule } from '@app/shared/modules/allmaterial/allmaterial.module';
+import * as _ from 'lodash';
 
 
 @Component({
@@ -12,66 +13,120 @@ import { AllAngularMaterialMDCModulesModule } from '@app/shared/modules/allmater
   styleUrl: './multilangcontrol.component.scss'
 })
 export class MultilangControlComponent implements AfterViewInit {
-  
-  formGroup: FormGroup = new FormGroup({
-    translatable: new FormControl(''),
-  })
+
+  formGroup: FormGroup = new FormGroup({})
+  formModel = {};
+  formModel2 = {};
 
   constructor(private cdr: ChangeDetectorRef, private fb: FormBuilder) {
-    const data = {
-      email: 'test@email.com',
-      password: 'test-password',
-      address: [
-        {
-          street: 'Street 1',
-          postCode: 1000
-        },
-        {
-          street: null,
-          postCode: null
+
+    this.formModel = {
+      A: {
+        AB: ''
+      },
+      B: {
+        BA: '',
+        BB: {
+          BBA: {
+            BBAA: '',
+            BBAB: ''
+          }
         }
-      ]
-    };
-
-    const validationSchema = {
-      email: [[Validators.required, Validators.email]],
-      password: [[Validators.required, Validators.minLength(6)]],
-      address: [
-        {
-          street: [[Validators.required]],
-          postCode: [[Validators.required]]
-        },
-        {
-          street: [[Validators.required]],
-          postCode: [[Validators.required]]
-        }
-      ]
-    };
-
-    this.formGroup = this.parseData(data, validationSchema) as FormGroup;
-    
-  }
-
-  ngAfterViewInit(): void {   
-
-    console.log('PARENTFORMGROP ', this.formGroup, this.formGroup.getRawValue());
-
-  }
- 
-
-    parseData(data: any, validators?: any): FormGroup | FormArray | [any, ValidatorFn[], AsyncValidatorFn[]] {
-      if (Array.isArray(data)) {
-        return this.fb.array(data.map((item, index) => this.parseData(item, (validators || {})[index])));
       }
-      if (typeof data === 'object' && data !== null) {
-        const formGroupContent = {};
-        for (const [key, value] of Object.entries(data)) {
-          formGroupContent[key] = this.parseData(value, (validators || {})[key]);
-        }
-        return this.fb.group(formGroupContent);
-      }
-      const [syncValidators = [], asyncValidators = []] = validators || [];
-      return [data, syncValidators, asyncValidators];
     }
+
+    this.buildFormGroup(this.formModel, this.formGroup);
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log('PARENTFORMGROP ', this.formGroup, this.formGroup.getRawValue());
+  }
+
+
+  buildFormGroup<FORMMODELTYPE>(formModel: FORMMODELTYPE, formGroup: FormGroup) {
+
+    const getPaths = (item) => {
+      const iter = (r, p) => {
+        const keys = Object.keys(r);
+        if (keys.length) {
+          return keys.forEach(x => iter(r[x], p.concat(x)));
+        }
+        result.push(p);
+      }
+      const result = [];
+      iter(item, []);
+      return result;
+    }
+
+    const fullPaths = (paths: any[]) => {
+      const fullPaths = [];
+
+      paths.forEach((path) => {
+        let temp = '';
+        let count = 0;
+
+        for(let i=0; i<path.length; i++) {
+          for(let c=i; c<i+1; c++) {
+            temp += path[c] + '.';
+          }
+          count++;
+          fullPaths.push(temp);
+        }
+        console.log(temp)
+      })
+
+
+      return fullPaths
+
+    }
+
+    console.log('FULL ', fullPaths(getPaths(this.formModel)));
+
+    const loop = (obj, currentKey, level) => {
+
+      level = level || '';
+
+      for(const key in obj) {
+
+        if(Object.keys(obj[key]).length) {
+          console.log(level, key, 'haschilds')
+        }else {
+          console.log(level, key, 'nochilds')
+        }
+
+        if(typeof obj[key] === 'object') {
+
+          if(Array.isArray(obj[key])) {
+            obj[key].forEach(arrayItem => loop(arrayItem, key, level + '.'))
+          }else {
+            loop(obj[key], key, level + '.');
+          }
+
+        }else {
+
+        }
+
+
+      }
+
+    }
+
+
+    const paths = getPaths(this.formModel);
+    console.log('PATHS ', paths);
+
+    paths.forEach((path) => {
+      const currentPath = [];
+      path.forEach((pathItem) => {
+
+      })
+    })
+
+
+    console.log(_.flatten(this.formModel))
+
+  }
+
 
 }
