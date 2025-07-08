8165e37a3861a992e051a060945284b65e76e9ed
boendermaker <elektrobong@googlemail.com>
2025-07-09 00:25:06 +0200

    dynamic form fixed

--- src/app/views/dynamicform/dynamicform.component.ts
+++ src/app/views/dynamicform/dynamicform.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, FormGroup } from '@angular/forms';
 import { TFormGroupBuilderControl} from '@app/components/dynamicformgroup/formgroupbuilder.interface';
 import { ControlCheckboxModel } from '@app/components/dynamicformgroup/model/control.checkbox.model.class';
@@ -14,51 +15,65 @@ import { ControlTextModel } from '@app/components/dynamicformgroup/model/control
 })
 export class DynamicformComponent implements OnInit {
 
+  destroyRef = inject(DestroyRef);
+
   form: FormGroup = new FormGroup({});
 
   testControls: TFormGroupBuilderControl[] = [
 
     <TFormGroupBuilderControl>new ControlTextModel(
-      'Testinput', 
-      new FormControl(''), 
-      {value: 'Lorem'}, 
-      {label: 'Vorname'}, 
+      'Testinput',
+      new FormControl(''),
+      {value: 'Lorem'},
+      {label: 'Vorname'},
       0
     ),
     <TFormGroupBuilderControl>new ControlSelectModel(
-      'TestSelect', 
-      new FormControl(''), 
-      [{label: 'Selectitem1', value: 0}, {label: 'Selectitem2', value: 1}], 
-      {label: 'Vorname', selected: 1}, 
+      'TestSelect',
+      new FormControl(''),
+      [{label: 'Selectitem1', value: 0}, {label: 'Selectitem2', value: 1}],
+      {label: 'Vorname', selected: 1},
       1
     ),
     <TFormGroupBuilderControl>new ControlCheckboxModel(
-      'TestCheckbox', 
-      new FormControl(''), 
-      {value: 'Lorem'}, 
-      {label: 'Vorname', selected: false}, 
+      'TestCheckbox',
+      new FormControl(''),
+      {value: 'Lorem'},
+      {label: 'Vorname', selected: false},
       2
     ),
     <TFormGroupBuilderControl>new ControlRadioModel(
-      'TestRadio', 
-      new FormControl(''), 
-      [{label: 'TestRadio1', value: 0}, {label: 'TestRadio2', value: 1}], 
-      {label: 'Vorname', selected: 0}, 
+      'TestRadio',
+      new FormControl(''),
+      [{label: 'TestRadio1', value: 0}, {label: 'TestRadio2', value: 1}],
+      {label: 'Vorname', selected: 0},
       3
     ),
 
   ]
 
   constructor() {
-    console.log(
-
-      this.testControls
-
-    )
+    this.form
+    this.handleFormChange();
   }
 
   ngOnInit(): void {
-    console.log();
+    this.testControls.forEach((control) => {
+      this.form.addControl(control.name, control.instance);
+    })
+  }
+
+  handleFormChange(): void {
+    this.form.valueChanges
+    .pipe(takeUntilDestroyed(this.destroyRef))
+    .subscribe({
+      next: (value) => {
+        console.log('Form value changed:', value);
+      },
+      error: (error) => {
+        console.error('Error in form value changes:', error);
+      }
+    })
   }
 
 }
